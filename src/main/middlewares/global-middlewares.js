4666a001cc09746fc703d82fe4cb9dd46cb27c07
AdrielModollo <adrielmodollo@outlook.com>
2022-10-21 13:21:17 -0300

    feat: middlewares

---
+++ src/main/middlewares/global-middlewares.js
@@ -0,0 +1,10 @@
+const globalErrorsMiddleware = (req, res) => {
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+};
+
+module.exports = { globalErrorsMiddleware }
+
+
